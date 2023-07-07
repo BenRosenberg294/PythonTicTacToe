db540248f383eeb639661bb2a0ab565609adc13a
BenRosenberg294 <noreply>
2023-07-07 18:32:39 +0300

    Added some classes to help create a computer player

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,38 +1,57 @@
 symbols = {0:' ', 1:'X', 4:'O'}
 
+class Cell:
+    def __init__(self):
+        self.value = 0
+    
+    def get_value(self):
+        return self.value
+    
+    def set_value(self, value):
+        self.value = value
+
+    def is_empty(self):
+        return self.value == 0
+    
+class Line:
+    def __init__(self, cell_list):
+        self.cell_list = cell_list
+    
+    def get_line_value(self):
+        return sum([cell.get_value() for cell in self.cell_list])
+
+    
 class Game:
     def __init__(self):
-        self.grid = [[0 for j in range(3)] for i in range(3)]
+        self.grid = [[Cell() for j in range(3)] for i in range(3)]
         self.turn = 1
 
+        self.lines = [Line(self.grid[i]) for i in range(3)]
+        self.lines += [Line([self.grid[i][j] for i in range(3)]) for j in range(3)]
+        self.lines.append(Line([self.grid[i][i] for i in range(3)]))
+        self.lines.append(Line([self.grid[i][2-i] for i in range(3)]))
+
     def __repr__(self):        
-        return '\n' + '- + - + -\n'.join([' | '.join([symbols[self.grid[i][j]] for j in range(3)]) + '\n' for i in range(3)])
+        return '\n' + '- + - + -\n'.join([' | '.join([symbols[self.grid[i][j].get_value()] for j in range(3)]) + '\n' for i in range(3)])
     
     def is_spot_full(self, row, col):
-        return not self.grid[row][col] == 0
+        return not self.grid[row][col].is_empty()
     
     def mark_spot(self, row, col, player):
-        self.grid[row][col] = player ** 2
+        self.grid[row][col].set_value(player ** 2)
         self.turn += 1
 
     def check_win(self):
-        results = []
-
-        for i in range(3):
-            results.append(sum(self.grid[i]))
-            results.append(sum([self.grid[i][j] for j in range(3)]))
-        results.append(sum([self.grid[i][i] for i in range(3)]))
-        results.append(sum([self.grid[2-i][i] for i in range(3)]))
-
-        if 3 in results:
-            print(self)
-            print("Player 1 won!")
-            return True
-        
-        if 12 in results:
-            print(self)
-            print("Player 2 won!")
-            return True
+        for line in self.lines:
+            if line.get_line_value() == 3:
+                print(self)
+                print("Player 1 won!")
+                return True
+            
+            if line.get_line_value() == 12:
+                print(self)
+                print("Player 2 won!")
+                return True
 
         return False 
 
